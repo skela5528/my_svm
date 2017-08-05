dfe60356060039ab61600234733244a094c37c5c
skela5528 <skela@yandex.ru>
2017-08-05 16:49:20 +0300

    Repository created

---
+++ simple_svm.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  4 17:12:46 2017
+
+@author: alex
+"""
+
+import numpy as np
+import sklearn.datasets
+import matplotlib.pyplot as plt
+from sklearn import metrics
+
+def vis_data(data, labels):
+    colors_d = {0: 'g', -1: 'g', 1: 'b'}
+    colors = [colors_d[x] for x in labels]
+    plt.scatter(data[:, 0], data[:, 1],
+                marker='o',
+                s=100,
+                edgecolors=colors,
+                c='none',
+                alpha=0.7,
+                linewidths=2)
+    plt.show()
+
+d, l = sklearn.datasets.make_blobs(centers=2, cluster_std=2.5)
+
+
+class simple_svm:
+    x = None
+    y = None
+    n = None
+    _kernel_type = ''
+    alphas = None
+    b = None
+    C = None
+    support_vectors = None
+    tolerance = None
+    max_passes = None
+    _random_seed = None
+
+    def __init__(self, C=1.0, tolerance=0.1, max_passes=20, kernel='linear', random_seed=1, verbose=0):
+        self.C = C
+        self.tolerance = tolerance
+        self.max_passes = max_passes
+        self._kernel_type = kernel
+        self._random_seed = random_seed
+        self.verbose = verbose
+
+    def fit(self, x, y):
+        self._verify_data(x, y)
+
+        self.x = x
+        self.y = y
+        self.n = len(y)
+        if self._random_seed:
+            np.random.seed(self._random_seed)
+
+        self.alphas = np.zeros(self.n)
+        self.b = 0
+        passes_count = 0
+
+        while (passes_count < self.max_passes):
+            num_changed_alphas = 0
+            # for each instance
+            for i in range(self.n):
+                E_i = self._calc_f_x(self.x[i]) - self.y[i]
+                if (self.y[i] * E_i < -self.tolerance and self.alphas[i] < self.C) or (
+                            self.y[i] * E_i > self.tolerance and self.alphas[i] > 0):
+                    rand_ind = np.random.randint(low=0, high=10, size=2)
+                    j = rand_ind[0] if rand_ind[0] != i else rand_ind[1]
+
+                    E_j = self._calc_f_x(self.x[j]) - self.y[j]
+                    a_i, a_j = self.alphas[i], self.alphas[j]
+
+                    # L and H
+                    if self.y[i] != self.y[j]:
+                        L = max(0, self.alphas[j] - self.alphas[i])
+                        H = min(self.C, self.C + self.alphas[j] - self.alphas[i])
+                    else:
+                        L = max(0, self.alphas[j] + self.alphas[i] - self.C)
+                        H = min(self.C, self.alphas[j] + self.alphas[i])
+
+                    if L == H:
+                        continue
+
+                    # small eta
+                    h = 2 * self._kernel(self.x[i], self.x[j]) - \
+                        self._kernel(self.x[i], self.x[i]) - \
+                        self._kernel(self.x[j], self.x[j])
+                    if h >= 0:
+                        continue
+
+                    # new a_j
+                    self.alphas[j] = self.alphas[j] - (self.y[j] * (E_i - E_j)) / h
+                    if self.alphas[j] > H:
+                        self.alphas[j] = H
+                    elif L <= self.alphas[j] <= H:
+                        self.alphas[j] = self.alphas[j]
+                    elif self.alphas[j] < L:
+                        self.alphas[j] = L
+
+                    if abs(a_j - self.alphas[j]) < 10 ** -5:
+                        continue
+
+                    # new a_i
+                    self.alphas[i] = self.alphas[i] + self.y[i] * self.y[j] * (a_j - self.alphas[j])
+
+                    # new b
+                    b1 = self.b - E_i - self.y[i] * (self.alphas[i] - a_i) * self._kernel(self.x[i], self.x[i]) - \
+                         self.y[j] * (self.alphas[j] - a_j) * self._kernel(self.x[i], self.x[j])
+                    b2 = self.b - E_j - self.y[i] * (self.alphas[i] - a_i) * self._kernel(self.x[i], self.x[j]) - \
+                         self.y[j] * (self.alphas[j] - a_j) * self._kernel(self.x[j], self.x[j])
+
+                    if 0 < self.alphas[i] < self.C:
+                        self.b = b1
+                    elif 0 < self.alphas[j] < self.C:
+                        self.b = b2
+                    else:
+                        self.b = (b1 + b2) / 2.0
+
+                    num_changed_alphas += 1
+                    if self.verbose > 0:
+                        print("num_changed_alphas = %d" % num_changed_alphas)
+            if num_changed_alphas == 0:
+                passes_count += 1
+            else:
+                passes_count = 0
+
+        # save support vectors ids
+        self.support_vectors = np.where(self.alphas > 0)[0]
+
+    def predict(self, x):
+        fx = [self._calc_f_x(xi) for xi in x]
+        predictions = [-1 if fxi < 0 else 1 for fxi in fx]
+        return predictions
+
+    def get_tr_accuracy(self):
+        predictions = self.predict(self.x)
+        return metrics.accuracy_score(self.y, predictions)
+
+    def visualize_model(self):
+        colors_d = {0: 'g', -1: 'g', 1: 'b'}
+        colors = [colors_d[val] for val in self.y]
+        plt.scatter(self.x[:, 0], self.x[:, 1],
+                    marker='o',
+                    s=80,
+                    edgecolors=colors,
+                    c='none',
+                    alpha=0.7,
+                    linewidths=2)
+        a, b = self._get_sv_byclass()
+        plt.plot(a[0], a[1], '--')
+        plt.plot(b[0], b[1], '--')
+        plt.show()
+
+    def _calc_f_x(self, x_val):
+        kernels_v = np.array([self._kernel(x_val, self.x[x]) if self.alphas[x] else 0 for x in range(self.n)])
+        f_x = sum(self.alphas * self.y * kernels_v) + self.b
+        return f_x
+
+    def _kernel(self, a, b):
+        if self._kernel_type == 'linear':
+            return sum(a * b)
+        else:
+            raise Exception("Unsuported kernel")
+            return None
+
+    def _get_sv_byclass(self):
+        sv_x = [self.x[xi] for xi in self.support_vectors]
+        sv_classes = [self.y[xi] for xi in self.support_vectors]  # self.predict(sv_x)
+
+        a = [sv_x[i] for i, pred in enumerate(sv_classes) if pred==-1]
+        b = [sv_x[i] for i, pred in enumerate(sv_classes) if pred==1]
+
+        return a, b
+
+    def _verify_data(self, x, y):
+        if len(x) != len(y):
+            print("X and Y contain different number of samples!")
+            return False
+        if len(np.unique(l)) > 2:
+            print("Currently only 2 classes support!")
+            return False
+        if 0 in y:
+            y[y == 0] = -1
+            return True
+
+
+
+
